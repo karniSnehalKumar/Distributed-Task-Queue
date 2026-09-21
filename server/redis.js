c7ba5f2d28f6060fbeebb541e3092f4102fd3114
Karni Snehal Kumar <145927889+karniSnehalKumar@users.noreply.github.com>
2026-09-21 11:15:53 +0530

    made changes in redis config

--- server/redis.js
+++ server/redis.js
@@ -4,7 +4,6 @@ const redis = new Redis({
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: parseInt(process.env.REDIS_PORT) || 6379,
   maxRetriesPerRequest: 3,
-  lazyConnect: false,
 });
 
 redis.on('connect', () => {
